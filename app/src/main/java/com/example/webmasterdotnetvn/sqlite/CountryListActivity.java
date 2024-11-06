388db53226350e7cbdc00bc584be2f0eab9c4b6c
thtnv <thtnv9876@gmail.com>
2024-11-06 08:17:54 +0700

    SÆQLite

---
+++ app/src/main/java/com/example/webmasterdotnetvn/sqlite/CountryListActivity.java
@@ -0,0 +1,80 @@
+package com.example.webmasterdotnetvn.sqlite;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CountryListActivity extends AppCompatActivity {
+
+    private DBManager dbManager;
+    private ListView listView;
+    private SimpleCursorAdapter adapter;
+    final String[] from = new String[] { DatabaseHelper._ID, DatabaseHelper.SUBJECT, DatabaseHelper.DESC };
+    final int[] to = new int[] { R.id.id, R.id.title, R.id.desc };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_emp_list);
+
+        dbManager = new DBManager(this);
+        dbManager.open();
+        Cursor cursor = dbManager.fetch();
+
+        listView = findViewById(R.id.list_view);
+        listView.setEmptyView(findViewById(R.id.empty));
+
+        adapter = new SimpleCursorAdapter(this, R.layout.activity_view_record, cursor, from, to, 0);
+        adapter.notifyDataSetChanged();
+
+        listView.setAdapter(adapter);
+
+        // OnClickListener for List Items
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long viewId) {
+                TextView idTextView = view.findViewById(R.id.id);
+                TextView titleTextView = view.findViewById(R.id.title);
+                TextView descTextView = view.findViewById(R.id.desc);
+
+                String id = idTextView.getText().toString();
+                String title = titleTextView.getText().toString();
+                String desc = descTextView.getText().toString();
+
+                Intent modifyIntent = new Intent(getApplicationContext(), ModifyCountryActivity.class);
+                modifyIntent.putExtra("title", title);
+                modifyIntent.putExtra("desc", desc);
+                modifyIntent.putExtra("id", id);
+
+                startActivity(modifyIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.add_record) {
+            Intent addMem = new Intent(this, AddCountryActivity.class);
+            startActivity(addMem);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
